f7971aece3a1965ea7b6a88b5d81d0da1afbe8c6
dalis-ops <64962787+dalis-ops@users.noreply.github.com>
2022-06-18 21:14:31 +0530

    Project

---
+++ sanity_ecommerce/dist/static/js/app.bundle.js
@@ -0,0 +1,34 @@
+webpackJsonp([1],[
+/* 0 */
+/***/ (function(module, exports, __webpack_require__) {
+
+	(function webpackMissingModule() { throw new Error("Cannot find module \"D:\\React\\ecommerce_sanity_stripe\\sanity_ecommerce\\node_modules\\normalize.css\\normalize.css\""); }());
+	module.exports = __webpack_require__(1);
+
+
+/***/ }),
+/* 1 */
+/***/ (function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	var _react = __webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \"react\""); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
+
+	var _react2 = _interopRequireDefault(_react);
+
+	var _reactDom = __webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \"react-dom\""); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
+
+	var _reactDom2 = _interopRequireDefault(_reactDom);
+
+	var _sanityRoot = __webpack_require__(!(function webpackMissingModule() { var e = new Error("Cannot find module \"part:@sanity/base/sanity-root\""); e.code = 'MODULE_NOT_FOUND'; throw e; }()));
+
+	var _sanityRoot2 = _interopRequireDefault(_sanityRoot);
+
+	function _interopRequireDefault(obj) {
+	  return obj && obj.__esModule ? obj : { default: obj };
+	}
+
+	_reactDom2.default.render(_react2.default.createElement(_sanityRoot2.default, null), document.getElementById('sanity'));
+
+/***/ })
+]);
